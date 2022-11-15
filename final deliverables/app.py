a942767617540c33b072b9e6672461ee634d6f93
kamalraj-ten <kamalrajdhanakodi@gmail.com>
2022-11-15 22:15:20 +0530

    added predition page

--- final deliverables/app.py
+++ final deliverables/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, request, redirect
 import pandas as pd
 import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 
 app = Flask(__name__)
+app.config['SECRET KEY'] = "a5d6n3j4k5l6k7mn342nw3"
 
 @app.route("/") #home route
 def home():
@@ -21,6 +22,21 @@ def home():
     curr = getCurrentCrudeOilPrice()
     return render_template("main_page.html", labels=labels, values=values, current_price=curr)
 
+@app.route("/predict", methods=["GET","POST"])
+def predictPage():
+    if request.method == "POST":
+        day1 = request.form['day-1']
+        day2 = request.form['day-2']
+        day3 = request.form['day-3']
+        if not day1 or not day2 or not day3:
+            flash('Enter all the past 3 days value')
+        else:
+            day1, day2, day3 = float(day1), float(day2), float(day3)
+            price = getCurrentCrudeOilPrice([day1, day2, day3])
+            return render_template('prediction.html', price=price)
+    return render_template('prediction.html')
+    pass
+
 def getCrudeOilData(n = 100):
     file = "dataset\\Crude Oil Prices Daily.xlsx"
     df = pd.read_excel(file)
@@ -29,7 +45,7 @@ def getCrudeOilData(n = 100):
     values = list(df["Closing Value"])
     return labels[len(labels)-n:], values[len(values)-n:] # returning only the last n data
 
-def getCurrentCrudeOilPrice():
+def getCurrentCrudeOilPrice(prices = []):
     model_file = "crude_oil_model.h5"
     file = "dataset\\Crude Oil Prices Daily.xlsx"
 
@@ -41,8 +57,12 @@ def getCurrentCrudeOilPrice():
     scaler = MinMaxScaler(feature_range=(0,1))
     x = scaler.fit_transform(x)
 
-    data = np.reshape(x[len(x)-10:],(-1,1,10))
-
+    if len(prices) == 0:
+        data = np.reshape(x[len(x)-3:],(-1,1,3))
+    else:
+        prices = np.array(prices)
+        data = np.reshape(prices, (-1,1,3))
+    
     model = tf.keras.models.load_model(model_file)
     curr = model.predict(data)
     curr = scaler.inverse_transform(curr)
